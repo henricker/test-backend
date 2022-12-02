90586fef42ea9e5ae466129b65446285b7738fd8
henricker <henriquevieira@alu.ufc.br>
2022-12-02 16:29:42 -0300

    fix: add checkout

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,6 +3,6 @@ import { setupApp } from '../infra/http/express/config/app';
 
 const app = setupApp();
 
-app.listen(process.env.PORT, () =>
+app.listen(process.env.PORT || 3000, () =>
   console.log(`Server running at PORT ${process.env.PORT}`)
 );
